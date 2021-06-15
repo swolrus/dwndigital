58bf129cd9f8a1587013556b20e616e6c05f4f46
yafavedave <83103253+yafavedave@users.noreply.github.com>
2021-06-16 00:30:09 +0800

    cleanup

--- dwndigital.py
+++ dwndigital.py
@@ -1,7 +1,7 @@
 from app.app_factory import create_app
 import os, config
 
-configkey = os.environ.get('FLASK_ENV') or 'production'
+configkey = os.environ.get('FLASK_ENV') or 'development'
 app = create_app(config.get_env_obj(configkey))
 
 if __name__ == "__main__":
